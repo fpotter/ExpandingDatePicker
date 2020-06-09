cd8cbc7f3bd88dfd08869352f9d36d41a80c9f98
Sebastián Benítez <sebastian@ds9soft.com>
2020-06-09 11:44:39 -0300

    Add platform requirement

--- Package.swift
+++ Package.swift
@@ -3,11 +3,13 @@
 import PackageDescription
 
 let package = Package(
-  name: "ExpandingDatePicker",
-  products: [
-    .library(name: "ExpandingDatePicker", targets: ["ExpandingDatePicker"])
-  ],
-  targets: [
-    .target(name: "ExpandingDatePicker", path: "ExpandingDatePicker")
-  ]
-)
+    name: "ExpandingDatePicker",
+    platforms: [
+        .macOS(.v10_10)
+    ],
+    products: [
+        .library(name: "ExpandingDatePicker", targets: ["ExpandingDatePicker"])
+    ],
+    targets: [
+        .target(name: "ExpandingDatePicker", path: "ExpandingDatePicker")
+    ])
